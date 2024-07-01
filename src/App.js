6df50f2cd36823979193afe57786e6eea7b78539
Akash <chauhanakash5258@gmail.com>
2024-07-01 19:54:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import './comp.js'
 import './App.css';
+import {useState , useEffect} from 'react'
 
 function App() {
+  // let counter=0;
+
+  let [counter , setCounter]=useState(10);
+
+  function AddValue(){
+    // counter=counter+1;
+    setCounter(counter+1);
+    // console.log(counter);
+  }
+  var DecValue=()=>{
+    // counter=counter-1;
+    setCounter(counter-1);
+    // console.log(counter);
+  }
+
+  useEffect(()=>{
+    console.log("mounted");
+
+    return ()=>{
+      console.log('unmounted');
+    }
+  } , []);
+
+  useEffect(()=>{
+    console.log('updation');
+  } , [counter])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Counter Value : {counter}</h1>
+    <button onClick={AddValue}>Add Value</button>
+    <br/>
+    <button onClick={DecValue}>Dec value</button>
+    </>
   );
 }
+
 
 export default App;
---
+++ src/comp.js
@@ -0,0 +1,11 @@
+function comp(props){
+    return(
+        <>
+        <h1>Hello {props.aa}</h1>
+        <h2>Hello2 {props.bb}</h2>
+        </>
+    )
+}
+
+
+
